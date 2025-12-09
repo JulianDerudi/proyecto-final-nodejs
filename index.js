cf6fea1baff7a78486a5770e2610b5ec55d71607
JulianDerudi <julianderudi9@gmail.com>
2025-12-09 20:18:07 -0300

    clase 10 modelado de api rest

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get('/ping', (req, res) => {
-    res.send('/pong');
+app.get('/', (req, res) => {
+    res.send('<h1>BIENVENIDO</h1>');
+});
+
+
+const products = [
+    { id: 1, name: 'Producto 1', price: 100 },
+    { id: 2, name: 'Producto 2', price: 200 },
+    { id: 3, name: 'Producto 3', price: 300 },
+];
+
+app.get('/products', (req, res) => {
+    res.json(products);
 });
 
 
